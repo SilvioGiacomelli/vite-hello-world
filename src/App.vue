912d52200ca9d01d05b9677a65143ac1c5efc35b
SilvioGiacomelli <91.giacomelli.silvio@gmail.com>
2024-03-20 13:22:57 +0100

    [Hello world]

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  data(){
+    return{
+      title:'Hello world'
+    }
+  }
+}
+</script>
+<template>
+  <div>
+    <h1>{{ title }}</h1>    
+  </div>
+</template>
+<style>
+</style>
